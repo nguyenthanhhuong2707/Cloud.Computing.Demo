785973bdf8a1362d6cc82bdaac89e323e65003a8
nguyenthanhhuong2707 <huongnttgbh18388@fpt.edu.vn>
2020-05-20 10:30:23 +0700

    huong

---
+++ profile.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Profile</title>
+</head>
+<body>
+	<h1>Profile.php</h1>
+	<h2>Huong Nguyen</h2>
+	<img src="img/2.jpg" alt="">
+	
+</body>
+</html>
