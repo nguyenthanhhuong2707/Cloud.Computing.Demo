8bb9aa9e4284e1dc897991ceee10054ab4cfb186
nguyenthanhhuong2707 <huongnttgbh18388@fpt.edu.vn>
2020-05-20 10:14:49 +0700

    2php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>My first php page</h1>
+<?php
+	echo "Huong Nguyen";
+	echo "Change the fix bug";
+	echo "Cloud Computing";
+?>
+	
+</body>
+</html>
